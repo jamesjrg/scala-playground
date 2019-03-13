9491b1a76f01e3df6c3cf077acae5c96ac275a25
James Gregory <james.jrg@gmail.com>
2019-03-13 19:23:41 +0000

    Add a simplistic performance test for New Year Chaos solution which times out for large inputs

--- playground/src/test/scala/hackerrank/arrays/NewYearChaosSpec.scala
+++ playground/src/test/scala/hackerrank/arrays/NewYearChaosSpec.scala
@@ -3,7 +3,57 @@ package hackerrank.arrays
 import org.scalatest.FlatSpec
 import arrays.newyearchaos.Solution
 
+import scala.io.Source
+import scala.io.StdIn.readLine
+
+
+object FileUtil {
+  def using[A <: { def close(): Unit }, B](resource: A)(f: A => B): B =
+    try {
+      f(resource)
+    } finally {
+      resource.close()
+    }
+
+  def fileToArray(filename: String) = {
+    using (Source.fromFile(filename)) {
+      bufferedSource =>
+        bufferedSource.getLines.toArray
+    }
+  }
+
+  def fileToReader(filename: String) = {
+    Source.fromFile(filename).bufferedReader()
+  }
+}
+
+object PerformanceTestUtil {
+  def runWithTimeout[A](getResult: => A, milliseconds: Int) = {
+
+    class WorkAroundScalaNotHavingDefaultKeyword {
+      var a: A = _
+    }
+
+    var result = new WorkAroundScalaNotHavingDefaultKeyword().a
+
+    val thread = new Thread {
+      override def run() {
+        result = getResult
+      }
+    }
+
+    thread.start()
+    thread.join(milliseconds)
+
+    result
+  }
+
+}
+
 class NewYearChaosSpec extends FlatSpec {
+
+
+
 
   it should "handle unchanged lines" in {
     val input = "1 2 3 4 5"
@@ -20,9 +70,23 @@ class NewYearChaosSpec extends FlatSpec {
     assert(Solution.mapInputToOutput(input) == "Too chaotic")
   }
 
-  it should "handle awkward things" in {
+  // could be simplified
+  it should "handle someone bribing after they have themselves been bribed" in {
     val input = "1 2 5 3 7 8 6 4"
     assert(Solution.mapInputToOutput(input) == "7")
+  }
+
+  it should "run large difficult input in a reasonable span of time" in {
+    import java.nio.file.Paths
+    val path = Paths.get(System.getProperty("user.home"), "/oss/hackerrank_test_cases/new_year_chaos.txt")
+    val input = FileUtil.fileToReader(path.toString)
+    val numberOfTestsInFile = input.readLine.trim.toInt
+    val n = input.readLine.trim.toInt
+    var result = ""
+
+    PerformanceTestUtil.runWithTimeout(Solution.mapInputToOutput(input.readLine), 5000)
+
+    assert(result == "119847")
   }
 }
 
